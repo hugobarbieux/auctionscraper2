bfa6e850c65f93eaf3921a73f23116e6b7c90375
hugobarbieux <43614788+hugobarbieux@users.noreply.github.com>
2019-03-01 11:51:13 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,8 +9,9 @@ html = scraperwiki.scrape("https://www.sdlauctions.co.uk/property-list/")
 print(html)
 #
 # # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
+root = lxml.html.fromstring(html)
+matchedlinks = root.cssselect("li p a")
+print(matchedlinks)
 #
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
